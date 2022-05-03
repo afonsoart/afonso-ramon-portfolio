2765239ee01859c0594b9a1c31fe497751ec566b
Afonso Ramon de Araujo Teles <afonso1907@hotmail.com>
2022-05-03 16:57:54 -0300

    Initial commit

---
+++ precache-manifest.2c4000600c2a9ce2d8b01545c0ea22a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d2750176b46f2c1370a",
+    "url": "./static/css/main.ee4542e8.chunk.css"
+  },
+  {
+    "revision": "6d2750176b46f2c1370a",
+    "url": "./static/js/main.6d275017.chunk.js"
+  },
+  {
+    "revision": "55181ac774012b79c0f7",
+    "url": "./static/js/1.55181ac7.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "abe5556dd61bc8d7b905884d588f8650",
+    "url": "./index.html"
+  }
+];
